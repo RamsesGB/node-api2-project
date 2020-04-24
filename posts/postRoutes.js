1c50c9aa9935c355c628331fb2a46356f738e996
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-23 21:36:51 -0400

    adding validation to endpoints

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -5,9 +5,20 @@ const Posts = require("../data/db");
 
 // POST @ /api/posts
 router.post("/", (req, res) => {
-  Posts.insert(req.body)
+  const createdPost = req.body;
+
+  Posts.insert(createdPost)
     .then((post) => {
-      res.status(201).json(post);
+      console.log(createdPost)
+      if (createdPost.title || createdPost.contents == undefined) {
+        res
+          .status(400)
+          .json({
+            errorMessage: "Please provide title and contents for the post.",
+          });
+      } else {
+        res.status(201).json(post);
+      }
     })
     .catch((err) => {
       console.log(err);
@@ -46,27 +57,43 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
+
   Posts.findById(id)
     .then((post) => {
-      res.status(200).json(post);
+      if (post[0]) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
     })
     .catch((error) => {
       console.log(error);
       res
-        .status(404)
-        .json({ message: "The post with the specified ID does not exist." });
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
     });
 });
 
 router.get("/:id/comments", (req, res) => {
   const { id } = req.params;
+
   Posts.findPostComments(id)
     .then((comments) => {
-      res.status(200).json(comments);
+      if (comments[0]) {
+        res.status(200).json(comments);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).json({ message: "Error retrieving post" });
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
     });
   //?
 });
@@ -74,9 +101,17 @@ router.get("/:id/comments", (req, res) => {
 // DELETE @ /api/posts/:id
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
+
   Posts.remove(id)
     .then((post) => {
-      res.status(204).json(post);
+      if (post[0]) {
+        res.status(204).json(post);
+      } else {
+        res.status(404).json({
+          message:
+            "The post with the specified ID does not exist or has been deleted.",
+        });
+      }
     })
     .catch((err) => {
       console.log(err);
@@ -87,11 +122,26 @@ router.delete("/:id", (req, res) => {
 //PUT @ /api/posts/:id
 router.put("/:id", (req, res) => {
   const { id } = req.params;
-  Posts.update(id ,req.body)
+  const postUpdate = { ...req.body, id: req.params.id };
+
+  Posts.update(id, req.body)
     .then((post) => {
-      res.status(200).json(post);
+      console.log(req.body);
+      console.log(postUpdate.id);
+      if (postUpdate.id !== id || post == 0) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else if (postUpdate.title && postUpdate.contents == undefined) {
+        res.status(400).json({
+          errorMessage: "Please provide title and contents for the post.",
+        });
+      } else {
+        res.status(200).json(post);
+      }
     })
     .catch((err) => {
+      console.log(err);
       res
         .status(500)
         .json({ error: "The post information could not be modified." });
